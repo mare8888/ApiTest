99cd28012986fa4f39bff4dbedd6271fb7773939
a.maretskij <maretskij.a+g@gmail.com>
2024-02-15 10:53:59 +0100

    mobile3

--- src/test/java/mobile/search/SearchTest.java
+++ src/test/java/mobile/search/SearchTest.java
@@ -5,7 +5,6 @@ import mobile.po.SkipPage;
 import mobile.services.ArticleService;
 import mobile.services.SearchService;
 import org.testng.annotations.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SearchTest extends BaseTest {
@@ -13,11 +12,8 @@ public class SearchTest extends BaseTest {
     @Test
     void shouldBeArticleNameVisible() {
         new SkipPage().clickSkip();
-
         new SearchService().searchTest("Appium");
-
         new ArticleService().getArticle();
-
         var article3 = new ArticleService().getArticleNameSpain();
 
         assertThat(article3).as("Header").isEqualTo("Historia");
